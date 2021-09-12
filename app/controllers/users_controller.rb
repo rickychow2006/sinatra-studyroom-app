39ba2f466a3ea9b80cfaf62d017e8dfddb12afa4
rickychow2006 <rickychow0716@gmail.com>
2021-09-12 10:10:35 -0500

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,22 @@ class UserController < ApplicationController
         
     end
 
+    post '/login'  do
+
+        @user = User.find_by(email: params[:email])
+
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect 'users/#{@user.id}'
+        end 
+    end
+
     get '/signup' do
     
     end 
+
+    get '/users/:id' do
+        "this will be the user show route"
+    end
 
 end 
