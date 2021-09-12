74bc63869ed02e2908e9446cef1183ac357d2b0b
rickychow2006 <rickychow0716@gmail.com>
2021-09-12 10:21:30 -0500

    add logged_in and current_user helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
 
     post '/login'  do
 
-        @user = User.find_by(email: params[:email])
+        @user = User.find(email: params[:email])
 
         if @user.authenticate(params[:password])
             session[:user_id] = @user.id
@@ -21,6 +21,17 @@ class UserController < ApplicationController
 
     get '/users/:id' do
         "this will be the user show route"
-    end
+    
+    helpers do 
+
+        def logged_in?
+            !!current_user
+        end 
+        
+        def current_user
+            @current_user ||= User.find_by(id: session[:user_id])
+        end 
+
+        
 
 end 
