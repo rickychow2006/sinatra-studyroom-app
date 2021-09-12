659a1eefe011ee7963423d549467c7c208501862
rickychow2006 <rickychow0716@gmail.com>
2021-09-12 15:26:05 -0500

    set up route to Signup, Logout, and showpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,31 @@ class UserController < ApplicationController
     end
 
     get '/signup' do
-    
+        erb :signup
     end 
 
-    get '/users/:id' do
-        "this will be the user show route"
+    post '/users' do 
+        if params[:name] != "" && params[:email] !="" && params[:password] != ""
+            @user = User.create(params)
+            redirect '/users/#{@user.id}'
+        else
+            redirect '/signup'
+
+        end 
+
+    end 
+
+
+    get '/users/:slug' do
+        @user = User.find_by(id: params[:id])
+        erb :'/users/show'
+    end 
+
+    get '/logout' do
+        session.clear 
+        redirect '/'
+
+    end 
     
     helpers do 
 
@@ -31,7 +51,6 @@ class UserController < ApplicationController
         def current_user
             @current_user ||= User.find_by(id: session[:user_id])
         end 
-
-        
+    end 
 
 end 
