79442c34c86a0bbbc25e43d2b05c7ef0d4237401
rickychow2006 <rickychow0716@gmail.com>
2021-09-15 11:07:24 -0500

    add flshing messaging, helper methods, and routes scaffolded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,43 @@
 class UserController < ApplicationController
     
     get '/login' do
+        redirect_if_logged_in
         erb :login
     end
 
     post '/login'  do
-
         @user = User.find_by(email: params[:email])
-
-        if @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id 
+            flash[:message] = 'Welcome, #{@user.name}!'
             redirect "users/#{@user.id}"
+        else 
+            falsh[:errors] = 'Sorry the credentials you are using are invalid. Please update your credentials and try again.'
+            redirect '/login'
         end 
     end
 
     get '/signup' do
+        redirect_if_logged_in
         erb :signup
     end 
 
     post '/users' do 
-            @user = User.create(params)
+        @user = User.create(params)
+        if @user.save 
             session[:user_id] = @user.id
+            flash[:message] = 'You have successfuly created an account! Welcome, #{@user.name} to the Study Room Community!'
             redirect "/users/#{@user.id}"
-    
+        else
+            flash[:errors] = "Account creation failure: #{@user.errors.full_messages.to_sentence}"
+            redirect '/signup'
+        end 
     end 
 
     get '/users/:id' do
         @user = User.find_by(id: params[:id])
+        redirect_if_not_logged_in
+
         erb :'/users/show'
     end 
 
@@ -35,6 +46,5 @@ class UserController < ApplicationController
         redirect '/'
 
     end 
-
 
 end 
