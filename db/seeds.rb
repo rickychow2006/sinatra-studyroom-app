49fb60284a3867de6988777276d743ce095e5a04
rickychow2006 <rickychow0716@gmail.com>
2021-10-12 16:59:23 -0500

    updated bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 alex = User.create(name: "Alex", email: "alex0701901@gmail.com", password: "password")
 emily = User.create(name: "Emily", email: "emily901123@gmail.com", password: "password")
 
-StudyPost.create(u_name: alex.name, user_id: alex.id, date: "9/11/2021", description: "Study CPA for 3 hrs", study_time: "50mins")
+StudyPost.create(u_name: alex.name, user_id: alex.id, date: "9/11/2021", description: "Study CPA", study_time: "3hrs")
+
+StudyPost.create(u_name: emily.name, user_id: emily.id, date: "9/11/2021", description: "Preparing GRE", study_time: "1hrs")
+
 
